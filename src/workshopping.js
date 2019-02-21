7c0c862f510f8c3655dbee3416934ad75056fed4
serapath <dev@serapath.de>
2019-02-21 23:19:19 +0000

    ADD first draft workshopping

---
+++ src/workshopping.js
@@ -0,0 +1,44 @@
+console.log('@TODO: implement workshopping')
+
+/*
+
+// workshops are versioned by date/hash/semver?
+module.exports = {
+  requirements: [
+  ],
+  advancements: [
+  ]
+}
+// codecamp
+module.exports = {
+
+}
+
+// people X adding workshops to the internet
+1:B
+2:C
+3:A()(B,C)
+4:E
+5:F
+6:D()(E,F)
+7:J
+
+8:G()(D,A)
+
+
+// person 8 adds and loads workshop G
+<html>
+  <head><title> G </title></head>
+  <body>
+    <div class="requirements">
+      <iframe src="D"></iframe>
+      <iframe src="A"></iframe>
+    </div>
+  </body>
+</html>
+
+
+// codecamp needs to specifically add all workshops who are part of the code camp
+// codecamps are versioned by date/hash/semver?
+
+*/
